4d92ef212fa153fe58d0fe4282ded739c9a56e55
EmilioAngelM <77894543+EmilioAngelM@users.noreply.github.com>
2021-02-04 17:48:54 -0500

    Validate user inputs

--- game.py
+++ game.py
@@ -22,16 +22,34 @@ print(user_choice)
 print(f"You chose: {user_choice}")
 
 
-#simulating a computer input
+
 
 
 
 
 options = ["rock","paper", "scissors"]
+
+
+#validate the user selection
+#stop the program and not try to determine a winned is the user choice is invalid
+
+
+if user_choice not in options:
+    print("OOPS, wrong option. Please choose a valid option!")
+    exit()
+
+
+
+
+
+
+#simulating a computer input
+
 computer_choice = random.choice(options)
 print(f"The computer chose: {computer_choice}")
 
 
+#Determining who won
 #simulating a message that announces the winner
 
 if user_choice == computer_choice:
@@ -56,9 +74,4 @@ elif user_choice == "scissors" and computer_choice == "rock":
 
 
 exit()
-#Determining who won
 
-print("-------------------")
-print("Oh, the computer won. It's ok.")
-print("-------------------")
-print("Thanks for playing. Please play again!")
