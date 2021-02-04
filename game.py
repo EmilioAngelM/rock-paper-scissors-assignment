21296b7dd746be88059e678424af5df2b8a42ca8
EmilioAngelM <77894543+EmilioAngelM@users.noreply.github.com>
2021-02-04 17:35:54 -0500

    Simulating a computer choice and simulating a message that announces the winner

--- game.py
+++ game.py
@@ -29,7 +29,7 @@ print(f"The computer chose: {computer_choice}")
 
 
 
-
+#simulating a computer input
 options = ["rock","paper", "scissors"]
 computer_choice = random.choice(options)
 
@@ -37,7 +37,20 @@ computer_choice = random.choice(options)
 
 
 
-#simulating a computer input
+if user_choice == computer_choice:
+    print("It is a tie!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("Great Job! You WON")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("Oh, better luck next time. The computer won")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("Oh, better luck next time. The computer won")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("Great Job! You WON")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("Great Job! You WON")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("Oh, better luck next time. The computer won")
 
 
 
